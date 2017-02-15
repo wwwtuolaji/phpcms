204fb5803a2348b44ff67a6b0ed6cca0d6c69431
Jwwwtuolaji <hebei_jjc@163.com>
2017-02-15 21:58:04 +0800

    init @jjc first create

---
+++ install_package/caches/caches_commons/caches_data/sitelist.cache.php
@@ -0,0 +1,46 @@
+<?php
+return array (
+  1 => 
+  array (
+    'siteid' => '1',
+    'name' => '默认站点',
+    'dirname' => '',
+    'domain' => 'http://phpcms/install_package/',
+    'site_title' => 'PHPCMS演示站',
+    'keywords' => 'PHPCMS演示站',
+    'description' => 'PHPCMS演示站',
+    'release_point' => '',
+    'default_style' => 'default',
+    'template' => 'default',
+    'setting' => 'array (
+  \'upload_maxsize\' => \'2048\',
+  \'upload_allowext\' => \'jpg|jpeg|gif|bmp|png|doc|docx|xls|xlsx|ppt|pptx|pdf|txt|rar|zip|swf\',
+  \'watermark_enable\' => \'1\',
+  \'watermark_minwidth\' => \'300\',
+  \'watermark_minheight\' => \'300\',
+  \'watermark_img\' => \'/statics/images/water/mark.png\',
+  \'watermark_pct\' => \'85\',
+  \'watermark_quality\' => \'80\',
+  \'watermark_pos\' => \'9\',
+)',
+    'uuid' => '1cb3f35e-f375-11e6-8449-e0946749930e',
+    'url' => 'http://phpcms/install_package/',
+  ),
+  2 => 
+  array (
+    'siteid' => '2',
+    'name' => 'phpcms',
+    'dirname' => 'phpcms',
+    'domain' => 'http://phpcms/',
+    'site_title' => '',
+    'keywords' => '',
+    'description' => '',
+    'release_point' => '',
+    'default_style' => 'default',
+    'template' => 'default',
+    'setting' => '{"upload_maxsize":"2000","upload_allowext":"jpg|jpeg|gif|bmp|png|doc|docx|xls|xlsx|ppt|pptx|pdf|txt|rar|zip|swf","watermark_enable":"0","watermark_minwidth":"300","watermark_minheight":"300","watermark_img":"statics\\/images\\/water\\/http:\\/\\/phpcms\\/install_package\\/mark.gif","watermark_pct":"100","watermark_quality":"80","watermark_pos":"9"}',
+    'uuid' => '',
+    'url' => 'http://phpcms/',
+  ),
+);
+?>
---
+++ install_package/phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://phpcms/install_package/',
+    'authkey' => 'HkaLcuGpBfgTR9FXoDv92MBRpk91UtUO',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
